15d7009ecf8a654bc68eb59ce44ea5476c99de97
EC2 Default User <ec2-user@ip-172-31-10-254.us-east-2.compute.internal>
2021-10-09 05:41:17 +0000

    3回目

--- db/migrate/20211009012833_create_tasks.rb
+++ db/migrate/20211009012833_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :tittle
-      t.date :bigin
+      t.date :bigin 
       t.date :finish
       t.integer :allday
       t.text :memo
